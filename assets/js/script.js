129a98845dfdc0858d20e7ac67d35f4c9a30606f
adiaz <diazalbertojr@gmail.com>
2022-07-06 23:05:22 -0400

    added timer on JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,11 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 let shuffledQuestions, currentQuestionIndex
 
 
-startButton.addEventListener('click', startGame)
+startButton.addEventListener('click', startGame,)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame(){
     console.log('Started')
@@ -16,11 +20,46 @@ function startGame(){
     currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
+    countdown()
 }
 
 function setNextQuestion() {
     resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+// Timer function
+// function timer(){
+//     var sec = 30;
+//     var timer = setInterval(function(){
+//         document.getElementById('start-btn').innerHTML='00:'+sec;
+//         sec--;
+//         if (sec < 0) {
+//             clearInterval(timer);
+//         }
+//     }, 1000);
+// }
+
+function countdown(){
+    var timeLeft = 69
+    var timerEl = document.getElementById('countdown');
+
+
+
+    var timerId = setInterval(function () {
+        if (timeLeft > 0){
+            timerEl.textContent = 'Time left is' + timeLeft;
+            console.log('Time', timeLeft)
+        } else {
+            timeLeft.textContent = 'Time is over'
+            console.log('Time is over');
+            clearInterval(timerId)
+        }
+
+        console.log(timeLeft);
+
+        timeLeft = timeLeft - 1;
+    }, 1000);
 }
 
 function showQuestion(question){
@@ -38,6 +77,7 @@ function showQuestion(question){
 }
 
 function resetState(){
+    clearStatusClass(document.body)
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild
@@ -45,8 +85,33 @@ function resetState(){
     }
 }
 
-function selectAnswer(){
+function selectAnswer(e){
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
 
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
 const questions = [
@@ -56,5 +121,25 @@ const questions = [
             {text: 'answer...', correct: true},
             {text: 'wrong answer', correct: false}
         ]
-    }
+    },
+    {
+        question: 'What is...???',
+        answer: [
+            {text: 'answer...', correct: true},
+            {text: 'wrong answer', correct: false}
+        ] 
+    },
+    {
+        question: 'What is...???',
+        answer: [
+            {text: 'answer...', correct: true},
+            {text: 'wrong answer', correct: false}
+        ] 
+    },    {
+        question: 'What is...???',
+        answer: [
+            {text: 'answer...', correct: true},
+            {text: 'wrong answer', correct: false}
+        ] 
+    },
 ]
